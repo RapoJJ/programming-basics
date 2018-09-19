2d713a802f6dc495bcd2ec436a59e55cb5787477
udwee <jonttu.92@hotmail.com>
2018-09-19 10:00:23 +0300

    Task one done

---
+++ condition-tasks/IfTask1/IfTask1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace IfTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selvitetään onko annettu luku positiivinen, negatiivinen vai nolla");
+            Console.Write("Anna luku: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            if (number == 0)
+                Console.WriteLine($"Numero {number} on nolla!");
+            else if (number < 0)
+                Console.WriteLine($"Numero {number} on negatiivinen!");
+            else
+                Console.WriteLine($"Numero {number} on positiivinen");
+
+
+        }
+    }
+}
