d566b3c32d48583dd4bf39cf505a61dbc716061b
udwee <jonttu.92@hotmail.com>
2018-09-20 09:01:53 +0300

    If task 2, version 1 done

--- condition-tasks/IfTask2/IfTask2/Program.cs
+++ condition-tasks/IfTask2/IfTask2/Program.cs
@@ -6,7 +6,17 @@ namespace IfTask2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma selvittää onko luku pariton vai parillinen.");
+            Console.Write("Anna luku: ");
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+
+            if (number == 0)
+                Console.WriteLine($"Syöttämäsi luku {number} on nolla.");
+            else if (number % 2 == 0)
+                Console.WriteLine($"Syöttämäsi luku {number} on parillinen.");
+            else
+                Console.WriteLine($"Syöttämäsi luku {number} on pariton.");
         }
     }
 }
