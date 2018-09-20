9d3a9adeedebb60efbf994106870a390d1fc407d
Udwee <jonttu.92@hotmail.com>
2018-09-20 13:03:36 +0300

    Changed formatting

--- condition-tasks/IfTask3/IfTask3/Program.cs
+++ condition-tasks/IfTask3/IfTask3/Program.cs
@@ -6,35 +6,33 @@ namespace IfTask3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ohjelma selvittää onko luku positiivinen vai negatiivinen" +
-                "ja onko luku pariton vai parillinen");
-            Console.Write("Syötä luku: ");
-            string userInput = Console.ReadLine();
+            //Ohjelma selvittää onko luku positiivinen vai negatiivinen sekä onko luku pariton vai parillinen.
             int number = 0;
             string posOrNeg = "";
             string evenOrOdd = "";
+
+            Console.Write("Syötä luku: ");
+            string userInput = Console.ReadLine();
             bool isNumber = int.TryParse(userInput, out number);
 
             if (isNumber)
             {
-                if (number % 2 == 0)
-                    evenOrOdd = "parillinen";
-                else
-                    evenOrOdd = "pariton";
-
                 if (number == 0)
                     posOrNeg = "nolla";
                 else if (number > 0)
                     posOrNeg = "positiivinen";
                 else
                     posOrNeg = "pariton";
+
+                if (number % 2 == 0)
+                    evenOrOdd = "parillinen";
+                else
+                    evenOrOdd = "pariton";
             }
             else
                 Console.WriteLine("Syöte ei ole luku");
 
             Console.WriteLine($"Syöttämäsi luku {number} on {posOrNeg} ja {evenOrOdd}");
-
-
         }
     }
 }
