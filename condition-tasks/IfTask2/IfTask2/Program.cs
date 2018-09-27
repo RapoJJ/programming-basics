04899cf44a21d3e9359b123b9e6b8e5863ad809b
udwee <jonttu.92@hotmail.com>
2018-09-27 19:50:18 +0300

    Ticket Price, version 2

--- condition-tasks/IfTask2/IfTask2/Program.cs
+++ condition-tasks/IfTask2/IfTask2/Program.cs
@@ -5,12 +5,13 @@ namespace IfTask2
     class Program
     {
         static void Main(string[] args)
-        {
+        {   //Program determines if user gets discount for tickets or not
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             double ticketPrice = 16.00;
             double discount = 0;
 
-            Console.Write("Syötä ikä: ");
+            Console.WriteLine($"Lipun hinta ilman alennuksia on {ticketPrice}.");
+            Console.Write("Syötä ikä: ");           //Asking users age
             string userInput = Console.ReadLine();
             int userAge = int.Parse(userInput);
 
@@ -22,12 +23,12 @@ namespace IfTask2
                 discount = 0.5;
             else                    //If users age doesnt grant a discount
             {
-                Console.Write("Valitse sopiva kategoria\n1. Varusmies\n2. Opiskelija\n3. Opiskelija ja MTK jäsen\n" +
-                    "4. MTK jäsen\n5. Ei mikään\n");
+                Console.Write("1. Varusmies\n2. Opiskelija\n3. Opiskelija sekä MTK jäsen\n" +
+                    "4. MTK jäsen\n5. Ei mikään\nValitse sopiva kategoria (1-5): ");
                 string userInput1 = Console.ReadLine();
                 int userChoice = int.Parse(userInput1);
 
-                if (userChoice == 1)
+                if (userChoice == 1)            //Setting discount to predetermined value
                     discount = 0.5;
                 else if (userChoice == 2)
                     discount = 0.45;
@@ -37,14 +38,8 @@ namespace IfTask2
                     discount = 0.15;
                 else if (userChoice == 5)
                     discount = 0;
-                else
-                    Console.WriteLine("Väärä syöte");
-
-
             }
-            Console.WriteLine($"Lipunhintasi on {ticketPrice - ticketPrice * discount}€.");
-
-
+            Console.WriteLine($"Uusi lipunhinta on {ticketPrice - ticketPrice * discount}€."); //Printing new price
         }
     }
 }
