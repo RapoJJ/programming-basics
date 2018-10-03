33fdcdc609d20230966915ec6a4f134323a96282
Udwee <jonttu.92@hotmail.com>
2018-10-03 14:59:49 +0300

    LoopTask1-2 finished, version 1

--- loop-tasks/LoopTask1-2/LoopTask1-2/Program.cs
+++ loop-tasks/LoopTask1-2/LoopTask1-2/Program.cs
@@ -6,7 +6,20 @@ namespace LoopTask1_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma laskee N ensimmäistä lukua yhteen.");
+            Console.Write("Syötä luku: ");
+            string userInput = Console.ReadLine();
+            bool isNumber = int.TryParse(userInput, out int number);
+            int sum = 0;
+
+            if (isNumber && number >= 1)
+            {
+                for (int i = 1; i <= number; i++)
+                    sum = sum + i;
+                Console.WriteLine($"Vastaus: {sum}");
+            }
+            else
+                Console.WriteLine("Antamasi syöte ei kelpaa");
         }
     }
 }
