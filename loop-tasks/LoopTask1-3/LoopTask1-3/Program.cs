719c1658a1870b3111bce7d9e4505a49d5b8ec4e
Udwee <jonttu.92@hotmail.com>
2018-10-03 15:19:10 +0300

    LoopTask1-3 finished, version 1

--- loop-tasks/LoopTask1-3/LoopTask1-3/Program.cs
+++ loop-tasks/LoopTask1-3/LoopTask1-3/Program.cs
@@ -6,7 +6,24 @@ namespace LoopTask1_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma käyttäjän syöttämän luvun ensimmäisen parittoman ja parillisen lukujen summan. ");
+            Console.Write("Syötä luku: ");
+            string userInput = Console.ReadLine();
+            bool isNumber = int.TryParse(userInput, out int number);
+            int oddSum = 0;
+            int evenSum = 0;
+
+            if (isNumber && number >= 1)
+            {
+                for (int i = 1; i <= number; i++)
+                    if (i % 2 == 0)
+                        evenSum = evenSum+ i;
+                    else
+                        oddSum = oddSum + i;
+                Console.WriteLine($"Vastaus: Parittomien summa = {oddSum}, Parillisten summa = {evenSum}");
+            }
+            else
+                Console.WriteLine("Antamasi syöte ei kelpaa");
         }
     }
 }
