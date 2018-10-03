2e4d42447d0bdff79fa6914a58f98a8b0edadbfb
udwee <jonttu.92@hotmail.com>
2018-10-03 13:50:42 +0300

    LoopTask1 initialized

---
+++ loop-tasks/LoopTask1/LoopTask1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LoopTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ohjelma tulostaa käyttäjän antaman luvun kertoman.");
+            Console.Write("Syötä luku: ");
+            string userInput = Console.ReadLine();
+            int number = 0;
+            bool isNumber = int.TryParse(userInput, out number);
+
+
+            if (isNumber)
+                if (number >= 1)
+                    int factorial = number
+
+        }
+    }
+}
