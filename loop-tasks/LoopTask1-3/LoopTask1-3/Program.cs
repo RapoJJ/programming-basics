7499177f4842d055683be74fa401a4063300af26
udwee <jonttu.92@hotmail.com>
2018-10-04 09:43:26 +0300

    LoopTask1-3 edited code, version 1.1

--- loop-tasks/LoopTask1-3/LoopTask1-3/Program.cs
+++ loop-tasks/LoopTask1-3/LoopTask1-3/Program.cs
@@ -6,20 +6,23 @@ namespace LoopTask1_3
     {
         static void Main(string[] args)
         {
+            int oddSum = 0;
+            int evenSum = 0;
+
             Console.WriteLine("Ohjelma käyttäjän syöttämän luvun ensimmäisen parittoman ja parillisen lukujen summan. ");
             Console.Write("Syötä luku: ");
             string userInput = Console.ReadLine();
             bool isNumber = int.TryParse(userInput, out int number);
-            int oddSum = 0;
-            int evenSum = 0;
 
             if (isNumber && number >= 1)
             {
                 for (int i = 1; i <= number; i++)
+                {
                     if (i % 2 == 0)
-                        evenSum = evenSum+ i;
+                        evenSum = evenSum + i;
                     else
                         oddSum = oddSum + i;
+                }
                 Console.WriteLine($"Vastaus: Parittomien summa = {oddSum}, Parillisten summa = {evenSum}");
             }
             else
