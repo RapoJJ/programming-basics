c85fa4798bfedbd3761aca9fde5e0c755ad94ae4
udwee <jonttu.92@hotmail.com>
2018-10-04 09:24:21 +0300

    LoopTask2-2 version 1 done

--- loop-tasks/LoopTask2-2/LoopTask2-2/Program.cs
+++ loop-tasks/LoopTask2-2/LoopTask2-2/Program.cs
@@ -6,7 +6,19 @@ namespace LoopTask2_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma tulostaa kertotaulun luvuille 1-9.");
+            int numberOne = 1;
+            int numberTwo = 1;
+            int answer = 1;
+
+            for (numberOne = 1; numberOne <= 9; numberOne++)
+            {
+                for (numberTwo = 1; numberTwo <= 9; numberTwo++)
+                {
+                    answer = numberOne * numberTwo;
+                    Console.WriteLine($"{numberOne} x {numberTwo} = {answer}");
+                }
+            }
         }
     }
 }
