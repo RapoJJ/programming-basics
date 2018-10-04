51403f8224e5dc73a1ad87c474294887edb7794a
udwee <jonttu.92@hotmail.com>
2018-10-04 08:53:50 +0300

    LoopTask2-1 version 1 done

---
+++ loop-tasks/LoopTask2-1/LoopTask2-1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LoopTask2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa kokonaisluvut 1-10 ja niiden neliöjuuret");
+            Console.WriteLine("Luku   Neliöjuuri");
+            double square = 1;
+            double i = 1;
+
+            for (i = 1; i <= 10; i++)
+            { 
+                square = Math.Sqrt(i);
+                Console.WriteLine($"{i}  {square}");
+            }
+        }
+    }
+}
