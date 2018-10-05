ca229cc390e91352eff34324e8a521bb5fca7ebe
udwee <jonttu.92@hotmail.com>
2018-10-05 13:41:26 +0300

    Added comments

--- loop-tasks/LoopTask1-4/LoopTask1-4/Program.cs
+++ loop-tasks/LoopTask1-4/LoopTask1-4/Program.cs
@@ -7,17 +7,17 @@ namespace LoopTask1_4
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma laskee N ensimmäistä lukua yhteen myös negatiivisilla numeroilla.");
-            Console.Write("Syötä luku: ");
+            Console.Write("Syötä luku: ");      //Asking user for input
             string userInput = Console.ReadLine();
-            bool isNumber = int.TryParse(userInput, out int number);
+            bool isNumber = int.TryParse(userInput, out int number);    //Checking if the input was number
             int sum = 0;
 
-            if (isNumber)
+            if (isNumber)       //Doing the loop if the input was number
             {
-                if (number > 0)
-                    for (int i = 0; i <= number; i++)
+                if (number > 0)         //If the number was positive
+                    for (int i = 0; i <= number; i++)       
                         sum = sum + i;
-                else
+                else                    //If the number was negative
                     for (int i = 0; i >= number; i--)
                         sum = sum + i;
                 Console.WriteLine($"Vastaus: {sum}");
