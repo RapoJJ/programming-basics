dcb8ccf6205af355e7f19751e324d11176378261
udwee <jonttu.92@hotmail.com>
2018-10-05 13:30:14 +0300

    Added comments

--- loop-tasks/LoopTask1-2/LoopTask1-2/Program.cs
+++ loop-tasks/LoopTask1-2/LoopTask1-2/Program.cs
@@ -7,12 +7,12 @@ namespace LoopTask1_2
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma laskee N ensimmäistä lukua yhteen.");
-            Console.Write("Syötä luku: ");
+            Console.Write("Syötä luku: ");      //Asking user for input
             string userInput = Console.ReadLine();
-            bool isNumber = int.TryParse(userInput, out int number);
+            bool isNumber = int.TryParse(userInput, out int number);    //checking if input is number
             int sum = 0;
 
-            if (isNumber && number >= 1)
+            if (isNumber && number >= 1)    //doing the loop if input was number and atleast 1
             {
                 for (int i = 1; i <= number; i++)
                     sum = sum + i;
