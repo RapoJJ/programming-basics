db6f28e56665b3226fad55d530418481e603d6d6
udwee <jonttu.92@hotmail.com>
2018-10-05 13:25:15 +0300

    Edited print and added comments

--- loop-tasks/LoopTask1-1/LoopTask1.1/Program.cs
+++ loop-tasks/LoopTask1-1/LoopTask1.1/Program.cs
@@ -6,19 +6,19 @@ namespace LoopTask1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ohjelma tulostaa käyttäjän kysytyn luvun kertoman.");
-            Console.Write("Syötä luku: ");
+            Console.WriteLine("Ohjelma tulostaa käyttäjältä kysytyn luvun kertoman.");
+            Console.Write("Syötä luku: ");      //Asking user to input a number
             string userInput = Console.ReadLine();
-            bool isNumber = int.TryParse(userInput, out int number);
+            bool isNumber = int.TryParse(userInput, out int number); //checking if input is number
             int answer = 1;
 
-            if (isNumber && number >= 1)
+            if (isNumber && number >= 1)    //only doing the loop if the input is number and is atleast 1
             {
                 for (int i = 1; i <= number; i++)
                     answer = answer * i;
                 Console.WriteLine($"Antamasi luvun ({userInput}) kertoma: {answer}");
             }
-            else
+            else     //input was wrong
                 Console.WriteLine("Antamasi syöte ei kelpaa");
         }
     }
