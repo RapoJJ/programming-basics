d42c92a704d8f4c96b1051a9392cb5200fe991e0
Udwee <jonttu.92@hotmail.com>
2018-10-09 15:39:44 +0300

    LoopTask2-4 version 1 done

--- loop-tasks/LoopTask2-4/LoopTask2-4/Program.cs
+++ loop-tasks/LoopTask2-4/LoopTask2-4/Program.cs
@@ -6,7 +6,34 @@ namespace LoopTask2_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma simuloi kolikon heittoa käyttäjältä kysytyn määrän verran ja ottaa tulokset talteen");
+
+            int i = 1;
+            int headsAmount = 0;
+            int tailsAmount = 0;
+            int throwResult = 1;
+            Random rnd = new Random();
+
+            Console.Write("Syötä heittojen määrä: ");
+            string userInput = Console.ReadLine();
+            bool isNumber = int.TryParse(userInput, out int throwAmount);
+
+            if (isNumber && throwAmount > 0)
+            {
+                do
+                {
+                    throwResult = rnd.Next(2);
+                    //Console.WriteLine(throwResult);
+                    i++;
+                    if (throwResult == 0)
+                        tailsAmount++;
+                    else
+                        headsAmount++;
+                } while (i <= throwAmount);
+            }
+            else
+                Console.WriteLine("Syöte ei kelpaa.");
+            Console.WriteLine($"Klaavoja tuli {tailsAmount} ja kruunuja {headsAmount}.");
         }
     }
 }
