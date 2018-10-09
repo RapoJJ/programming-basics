dd6d322b310ef8bafd1ed69132e3e2457d105c68
Udwee <jonttu.92@hotmail.com>
2018-10-09 15:40:39 +0300

    Edited code and added comments

--- loop-tasks/LoopTask2-3/LoopTask2-3/Program.cs
+++ loop-tasks/LoopTask2-3/LoopTask2-3/Program.cs
@@ -13,17 +13,16 @@ namespace LoopTask2_3
 
             for (int i = 1; i <= 4; i++)
             {
-                Console.Write($"\nRivi {i} ");
+                Console.Write($"Rivi {i} ");
                 for (int j = 1; j <= 5; j++)
                 {
-                    number = rnd.Next(50);
-                    if (j == 5)
-                        Console.Write($"{number}");
+                    number = rnd.Next(51); //New random number [0-50]
+                    if (j == 5)         //If printing the last number of the row (Each row has 5 numbers)
+                        Console.WriteLine($"{number}");
                     else
                         Console.Write($"{number}, ");
                 }
             }
-            Console.WriteLine();
         }
     }
 }
