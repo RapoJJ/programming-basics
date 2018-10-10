d488f3e08b4cc1738b4ce3a2438f13aa7c5b650c
udwee <jonttu.92@hotmail.com>
2018-10-10 09:27:21 +0300

    LoopTask2-6 version 1 done

---
+++ loop-tasks/LoopTask2-6/LoopTask2-6/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LoopTask2_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Tee ohjelma, joka simuloi nopanheittoa 1000 kertaa ja tulostaa " +
+                "lisäksi kuutosen esiintymiskertojen lukumäärän.");
+
+            int i = 1;
+            int result = 1;
+            int countOfSix = 0;
+            Random rnd = new Random();
+
+
+            do
+            {
+                result = rnd.Next(1, 7);
+                if (result == 6)
+                    countOfSix++;
+                Console.WriteLine($"{i}. {result}");
+                i++;
+            } while (i <= 1000);
+            Console.WriteLine($"Kuutonen arvottiin {countOfSix} kertaa.");
+        }
+    }
+}
