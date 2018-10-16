226358a41a442202e38898703b4d85369200f022
udwee <jonttu.92@hotmail.com>
2018-10-16 14:21:21 +0300

    FunctionTask1 version 1 done

---
+++ function-tasks/FunctionTask1/FunctionTask1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FunctionTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //Programm prints out user defined amount of stars, uses function to write the stars
+            while (true)
+            {
+                int userInput;
+                Console.Write("Syötä tähtien määrä: ");
+                bool isNumber = int.TryParse(Console.ReadLine(), out userInput);
+
+                if (isNumber && userInput > 0)
+                {
+                    Console.WriteLine($"{amountOfStars(userInput)}");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ei sallittu syöte. Syötteen pitää olla positiivinen luku!");
+                }
+            }
+        }
+        static string amountOfStars(int amount)
+        {
+            string value = "";
+
+            for (int i = 0; i < amount; i++)
+            {
+                value += "*";
+            }
+            return value;
+        }
+    }
+}
