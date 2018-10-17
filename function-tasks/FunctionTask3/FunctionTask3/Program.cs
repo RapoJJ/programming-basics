080f19f54c4f7d5b22d680fac58c4f4b43b10640
udwee <jonttu.92@hotmail.com>
2018-10-17 14:07:57 +0300

    FunctionTask3 version 1 done

--- function-tasks/FunctionTask3/FunctionTask3/Program.cs
+++ function-tasks/FunctionTask3/FunctionTask3/Program.cs
@@ -6,7 +6,30 @@ namespace FunctionTask3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Käyttäjä pyydetään luku väliltä 1-20, lukua kysytään uudestaan jos se " +
+                "ei ole annetulta väliltä");
+            int number = numberFromRange(1, 20);
+            Console.WriteLine($"Syöttämäsi luku {number} on väliltä 1-20.");
+        }
+        static int numberFromRange(int lowerBound, int upperbound)
+        {
+            int allowedNumber;
+            while (true)
+            {
+                Console.Write("Syötä luku (1-20): ");
+                bool isNumber = int.TryParse(Console.ReadLine(), out int userInput);
+
+                if (isNumber && userInput > lowerBound && userInput < upperbound)
+                {
+                    allowedNumber = userInput;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Ei sallittu syöte");
+                }
+            }
+            return allowedNumber;
         }
     }
 }
