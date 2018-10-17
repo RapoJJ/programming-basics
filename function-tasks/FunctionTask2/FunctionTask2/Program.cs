9a74a65afc3300286384db7c5bbd44cf1193f923
udwee <jonttu.92@hotmail.com>
2018-10-17 13:04:56 +0300

    FunctionTask2 version 1 done

--- function-tasks/FunctionTask2/FunctionTask2/Program.cs
+++ function-tasks/FunctionTask2/FunctionTask2/Program.cs
@@ -6,7 +6,53 @@ namespace FunctionTask2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma pyytää käyttäjältä 2 lukua ja tulostaa niistä pienemmän ja isomman.");
+            while (true)
+            {
+                Console.Write("Syötä 1. luku: ");
+                bool isNumber1 = int.TryParse(Console.ReadLine(), out int userInput1);
+                Console.Write("Syötä 2. luku: ");
+                bool isNumber2 = int.TryParse(Console.ReadLine(), out int userInput2);
+
+                if (isNumber1 && isNumber2)
+                {
+                    Console.WriteLine($"Luku {smallerNumber(userInput1, userInput2)} on pienempi kuin " +
+                        $"{biggerNumber(userInput1, userInput2)}.");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Väärä syöte. Syötä numerot uudestaan!");
+                }
+            }
+        }
+        static int smallerNumber(int number1, int number2)
+        {
+            int smallerNumber;
+            
+            if (number1 > number2)
+            {
+                smallerNumber = number2;
+            }
+            else
+            {
+                smallerNumber = number1;
+            }
+            return smallerNumber;
+        }
+        static int biggerNumber(int number1, int number2)
+        {
+            int biggerNumber;
+
+            if (number1 > number2)
+            {
+                biggerNumber = number1;
+            }
+            else
+            {
+                biggerNumber = number2;
+            }
+            return biggerNumber;
         }
     }
 }
