2615538753293b5237fabbd2a598e503b5b51ed2
Udwee <jonttu.92@hotmail.com>
2018-10-25 13:28:52 +0300

    RecapTask1 version 1 done

--- recap-tasks/RecapTask1/RecapTask1/Program.cs
+++ recap-tasks/RecapTask1/RecapTask1/Program.cs
@@ -6,7 +6,20 @@ namespace RecapTask1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma, kysyy käyttäjältä sanan tai lauseen ja tulostaa sen 5 kertaa.");
+            string empty = "";
+            string printout = UserSentence(empty);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(printout);
+            }
+        }
+        static string UserSentence(string sentence)
+        {
+            Console.Write("Syötä haluamasi lause/sana: ");
+            sentence = Console.ReadLine();
+            return sentence;
         }
     }
 }
