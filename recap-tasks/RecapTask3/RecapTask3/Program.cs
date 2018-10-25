43518927b634402b1826e59142c506bfd28deddf
Udwee <jonttu.92@hotmail.com>
2018-10-25 13:45:16 +0300

    RecapTask3 version 1 done

--- recap-tasks/RecapTask3/RecapTask3/Program.cs
+++ recap-tasks/RecapTask3/RecapTask3/Program.cs
@@ -6,7 +6,28 @@ namespace RecapTask3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma kysyy käyttäjältä lukuja, kunnes hän syöttää luvun -1. Ohjelma tulostaa näytölle syötettyjen lukujen summan.");
+            int sum = 0;
+
+            while (true)
+            {
+                Console.Write("Syötä luku: ");
+                bool isNumber = int.TryParse(Console.ReadLine(), out int userInput);
+
+                if (isNumber && userInput != -1)
+                {
+                    sum += userInput;
+                }
+                else if (isNumber && userInput == -1)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Syötteen pitää olla luku.");
+                }
+            }
+            Console.WriteLine($"Lukujen summa on: {sum}");
         }
     }
 }
