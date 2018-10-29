507e480b2316470299cd002de131519fd03345a9
udwee <jonttu.92@hotmail.com>
2018-10-29 11:52:51 +0200

    FunctionTask4Class version 1 done

--- function-tasks/FunctionTask4Class/FunctionTask4Class/Program.cs
+++ function-tasks/FunctionTask4Class/FunctionTask4Class/Program.cs
@@ -6,20 +6,33 @@ namespace FunctionTask4Class
     {
         static void Main(string[] args)
         {
+            //int indx = 0;
+
             Console.WriteLine("Syötä 10 lukua.");
-            LargestNumber();
+            //Console.WriteLine($"Suurin luku {LargestNumber(ref indx)} oli {indx}.");
+            Console.WriteLine($"Suurin luku {LargestNumber(out int indx)} oli {indx}.");
         }
 
-        static int LargestNumber()
+        static int LargestNumber(out int indx)
         {
+            indx = 0;
+            int largestNumber = 0;
             for (int i = 0; i < 10; i++)
             {
                 Console.Write($"{i + 1}.");
                 int userNumber = int.Parse(Console.ReadLine());
-
+                if (userNumber < 0)
+                {
+                    Console.WriteLine("Väärä syöte, syötä positiivinen luku.");
+                    i--;
+                }
+                if (userNumber > largestNumber)
+                {
+                    largestNumber = userNumber;
+                    indx = i + 1;
+                }
             }
-            return 0;
+            return largestNumber;
         }
     }
-
 }
