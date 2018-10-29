872c8f7689e2cd750a1dc159b92b864c453c45bc
udwee <jonttu.92@hotmail.com>
2018-10-29 11:30:21 +0200

    FunctionTask4Class initialized

---
+++ function-tasks/FunctionTask4Class/FunctionTask4Class/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FunctionTask4Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Syötä 10 lukua.");
+            LargestNumber();
+        }
+
+        static int LargestNumber()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{i + 1}.");
+                int userNumber = int.Parse(Console.ReadLine());
+
+            }
+            return 0;
+        }
+    }
+
+}
