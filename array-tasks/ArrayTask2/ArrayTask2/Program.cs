c53b68c7b9a2a5ce2269e6d8441777a906f3f801
udwee <jonttu.92@hotmail.com>
2018-10-31 13:50:55 +0200

    ArrayTask2 version 1 done

--- array-tasks/ArrayTask2/ArrayTask2/Program.cs
+++ array-tasks/ArrayTask2/ArrayTask2/Program.cs
@@ -6,7 +6,19 @@ namespace ArrayTask2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int sum = 0;
+            int average;
+            int[] numbers = new int[100];
+            Random rnd = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(50);
+                sum += numbers[i];
+                Console.WriteLine($"{i + 1}. {numbers[i]}");
+            }
+            average = sum / numbers.Length;
+            Console.WriteLine($"\nKA. {average}");
         }
     }
 }
