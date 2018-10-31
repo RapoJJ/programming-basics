549fa6faf703bd4200971fe0815b370a9df1f799
udwee <jonttu.92@hotmail.com>
2018-10-31 13:04:31 +0200

    ArrayExample1 version 1 done

--- array-tasks/ArrayExample1/ArrayExample1/Program.cs
+++ array-tasks/ArrayExample1/ArrayExample1/Program.cs
@@ -8,6 +8,18 @@ namespace ArrayExample1
         {
             Console.WriteLine("Ohjelma alustaa kokonaisluku taulukon (10 alkiota), " +
                 "johon arvotaan lukuja 0 - 100 väliltä");
+            int[] numbers = new int[10];
+            Random rnd = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(100);
+            }
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+            Console.WriteLine("Ohjelman suoritus on päättynyt");
         }
     }
 }
