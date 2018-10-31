a113d42602e755cef84dcde28f5d853f5b04c44f
udwee <jonttu.92@hotmail.com>
2018-10-31 12:42:42 +0200

    ArrayExample1 initialized

---
+++ array-tasks/ArrayExample1/ArrayExample1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ArrayExample1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma alustaa kokonaisluku taulukon (10 alkiota), " +
+                "johon arvotaan lukuja 0 - 100 väliltä");
+        }
+    }
+}
