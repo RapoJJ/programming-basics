087b2c2e31bee1130d1034adb62ab6fb92489aa5
Udwee <jonttu.92@hotmail.com>
2018-11-11 20:22:36 +0200

    StringTask4 initialized

---
+++ string-tasks/StringTask4/StringTask4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringTask4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tarkastaa onko käyttäjältä kysytty sana tai lause palintromi.");
+        }
+    }
+}
