2f2ab04b24860c9074615732107cb5ce691dcb6f
Udwee <jonttu.92@hotmail.com>
2018-11-11 20:22:02 +0200

    StringTask2 initialized

---
+++ string-tasks/StringTask2/StringTask2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma muuttaa käyttäjän syöttämästä syötteestä e kirjaimet @ merkiksi.");
+        }
+    }
+}
