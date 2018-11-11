dde003c0ae8f6d36088812a83b5f4e52653b3735
Udwee <jonttu.92@hotmail.com>
2018-11-11 22:45:02 +0200

    StringTaskVocalCalc initialized

---
+++ string-tasks/StringTaskVocalCalc/StringTaskVocalCalc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringTaskVocalCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee käyttäjän syöttämästä tiedosta vokaalien lukumäärän.");
+        }
+    }
+}
