1d383e81e19a19958b2500511d6c1ffa42a77d10
Udwee <jonttu.92@hotmail.com>
2018-11-11 21:48:04 +0200

    StringTask3 version 1 done

--- string-tasks/StringTask3/StringTask3/Program.cs
+++ string-tasks/StringTask3/StringTask3/Program.cs
@@ -7,6 +7,36 @@ namespace StringTask3
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma laskee käyttäjän syöttämästä syötteestä vaikkapa L kirjainten lukumäärän.");
+            string strInput = UserInput();
+            char countThis = ChooseChar();
+            Console.WriteLine($"Syöte: {strInput}\nSyötteessä on {CountOfChar(ref strInput, ref countThis)} {countThis} kirjainta");
+        }
+        static string UserInput()
+        {
+            Console.Write("Syötä lause: ");
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+
+        static int CountOfChar(ref string x, ref char y)
+        {
+            int count = 0;
+            x = x.ToLower();
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (x[i] == y)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        static char ChooseChar ()
+        {
+            Console.Write("Syötä kirjain, jonka lukumäärä lasketaan: ");
+            char UserInput = Char.ToLower(Console.ReadKey().KeyChar);
+            Console.WriteLine();
+            return UserInput;
         }
     }
 }
