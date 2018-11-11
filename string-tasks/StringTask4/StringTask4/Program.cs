44139461235f798aa386ae391c694bebe6c4a5b1
Udwee <jonttu.92@hotmail.com>
2018-11-11 22:32:02 +0200

    StringTask4 version 1 done

--- string-tasks/StringTask4/StringTask4/Program.cs
+++ string-tasks/StringTask4/StringTask4/Program.cs
@@ -6,7 +6,37 @@ namespace StringTask4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Ohjelma tarkastaa onko käyttäjältä kysytty sana tai lause palintromi.");
+            Console.WriteLine("Ohjelma tarkastaa onko käyttäjältä kysytty sana tai lause palindromi.");
+            string userInput = UserInput();
+
+            if (IsPalindrome(userInput))
+            {
+                Console.WriteLine($"Sana {userInput} on palindromi.");
+            }
+            else
+            {
+                Console.WriteLine($"Sana {userInput} ei ole palindromi.");
+            }
+
+        }
+        static string UserInput()
+        {
+            Console.Write("Syötä lause: ");
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+        static bool IsPalindrome(string str)
+        {
+            str = str.ToUpper();
+            str = str.Replace(" ", "");
+            for (int i = 0, j = str.Length - 1; i < j; i++, j--)
+            {
+                if (str[i] != str[j])
+                {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
