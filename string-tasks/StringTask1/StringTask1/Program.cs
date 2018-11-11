2675c33f97557f176f6dac2d34ec1d749e79c99b
Udwee <jonttu.92@hotmail.com>
2018-11-11 20:21:46 +0200

    StringTask1 initialized

---
+++ string-tasks/StringTask1/StringTask1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma ilmoittaa käyttäjän syöttämästä syötteestä merkkien lukumäärän.");
+        }
+    }
+}
