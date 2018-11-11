8d2d1d8168cdbac46a72f3c18efaa021130a5db0
Udwee <jonttu.92@hotmail.com>
2018-11-11 21:13:22 +0200

    StringTask2 version 1 done

--- string-tasks/StringTask2/StringTask2/Program.cs
+++ string-tasks/StringTask2/StringTask2/Program.cs
@@ -7,6 +7,21 @@ namespace StringTask2
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma muuttaa käyttäjän syöttämästä syötteestä e kirjaimet @ merkiksi.");
+
+            string input = UserInput();
+
+            Console.WriteLine($"Syöte: {input}\nMuutettu syöte: {ReplacedString(ref input)}");
+        }
+        static string UserInput()
+        {
+            Console.Write("Syötä lause: ");
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+        static string ReplacedString (ref string x)
+        {
+            string stringNew = x.Replace("e", "@");
+            return stringNew;
         }
     }
 }
