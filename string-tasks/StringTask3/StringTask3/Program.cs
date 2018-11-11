1ebbe673da7a9022f8bb645457cef8505da0aaa9
Udwee <jonttu.92@hotmail.com>
2018-11-11 20:22:20 +0200

    StringTask3 initialized

---
+++ string-tasks/StringTask3/StringTask3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StringTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee käyttäjän syöttämästä syötteestä vaikkapa L kirjainten lukumäärän.");
+        }
+    }
+}
