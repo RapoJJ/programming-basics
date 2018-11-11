dc659bbaf96b00a925e92e98505b82d0200a82e4
Udwee <jonttu.92@hotmail.com>
2018-11-11 21:01:10 +0200

    StringTask1 version 1 done

--- string-tasks/StringTask1/StringTask1/Program.cs
+++ string-tasks/StringTask1/StringTask1/Program.cs
@@ -7,6 +7,22 @@ namespace StringTask1
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma ilmoittaa käyttäjän syöttämästä syötteestä merkkien lukumäärän.");
+            string strInput = UserInput();
+
+            Console.WriteLine($"Syötteesi oli {strInput}.\nSiinä on {LengthOfString(ref strInput)} merkkiä.");
+
+        }
+        static string UserInput()
+        {
+            Console.Write("Syötä lause: ");
+            string userInput = Console.ReadLine();
+            return userInput;
+        }
+
+        static int LengthOfString(ref string x)
+        {
+            int length = x.Length;
+            return length;
         }
     }
 }
