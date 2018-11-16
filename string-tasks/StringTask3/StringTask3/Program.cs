430c7b370a45aa17d46fc14749918a39c709dfd6
udwee <jonttu.92@hotmail.com>
2018-11-16 09:25:09 +0200

    Added summaries to functions.

--- string-tasks/StringTask3/StringTask3/Program.cs
+++ string-tasks/StringTask3/StringTask3/Program.cs
@@ -9,15 +9,27 @@ namespace StringTask3
             Console.WriteLine("Ohjelma laskee käyttäjän syöttämästä syötteestä vaikkapa L kirjainten lukumäärän.");
             string strInput = UserInput();
             char countThis = ChooseChar();
-            Console.WriteLine($"Syöte: {strInput}\nSyötteessä on {CountOfChar(strInput, countThis)} {countThis} kirjainta");
+            Console.WriteLine($"Syöte: {strInput}\nSyötteessä on {CountOfChar(strInput, countThis)} {countThis} kirjainta.");
         }
+        /// <summary>
+        /// Asks user for input and saves it as string.
+        /// </summary>
+        /// <returns></returns>
         static string UserInput()
         {
             Console.Write("Syötä lause: ");
             string userInput = Console.ReadLine();
             return userInput;
         }
-
+        /// <summary>
+        /// Counts how many characters there is in a string.
+        /// x = any string
+        /// y = any char
+        /// Returns number of the characters.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns></returns>
         static int CountOfChar(string x, char y)
         {
             int count = 0;
@@ -31,6 +43,11 @@ namespace StringTask3
             }
             return count;
         }
+        /// <summary>
+        /// Asks user to input character.
+        /// Returns user chosen character.
+        /// </summary>
+        /// <returns></returns>
         static char ChooseChar ()
         {
             Console.Write("Syötä kirjain, jonka lukumäärä lasketaan: ");
