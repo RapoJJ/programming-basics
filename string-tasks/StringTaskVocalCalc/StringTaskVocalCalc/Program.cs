914e0e36f0f2e170aba3cd97dd4d70c975f9149a
udwee <jonttu.92@hotmail.com>
2018-11-16 09:36:18 +0200

    Added summaries to functions. Changed "sana" to "syöte" in print.

--- string-tasks/StringTaskVocalCalc/StringTaskVocalCalc/Program.cs
+++ string-tasks/StringTaskVocalCalc/StringTaskVocalCalc/Program.cs
@@ -8,19 +8,30 @@ namespace StringTaskVocalCalc
         {
             Console.WriteLine("Ohjelma laskee käyttäjän syöttämästä tiedosta vokaalien lukumäärän.");
             string word = UserInput();
-            Console.WriteLine($"Sanassa {word} on {VowelCount(word)} vokaalia.");
+            Console.WriteLine($"Syötteessä '{word}' on {VowelCount(word)} vokaalia.");
 
             //Console.WriteLine($"Sanassa {word} on {word.Length - VowelsRemoved(word).Length} vokaalia.");
 
             //Console.WriteLine($"Sanassa {word} on {OnlyVowels(word).Length} vokaalia.");
 
         }
+        /// <summary>
+        /// Asks user for input and saves it as string.
+        /// </summary>
+        /// <returns></returns>
         static string UserInput()
         {
             Console.Write("Syötä lause: ");
             string userInput = Console.ReadLine();
             return userInput;
         }
+        /// <summary>
+        /// Counts how many vowels there are in a string.
+        /// Returns count of vowels as int.
+        /// sentence = any string
+        /// </summary>
+        /// <param name="sentence"></param>
+        /// <returns></returns>
         static int VowelCount(string sentence)
         {
             sentence = sentence.ToLower();
@@ -34,6 +45,13 @@ namespace StringTaskVocalCalc
             }
             return count;
         }
+        /// <summary>
+        /// Removes vowels from string.
+        /// Returns string without vowels.
+        /// sentence = any string
+        /// </summary>
+        /// <param name="sentence"></param>
+        /// <returns></returns>
         static string VowelsRemoved(string sentence)
         {
             sentence = sentence.ToLower();
@@ -47,6 +65,13 @@ namespace StringTaskVocalCalc
             }
             return sentence;
         }
+        /// <summary>
+        /// Deletes anything that isn't vowel from string
+        /// Returns string with only vowels.
+        /// sentence = any string
+        /// </summary>
+        /// <param name="sentence"></param>
+        /// <returns></returns>
         static string OnlyVowels(string sentence)
         {
             sentence = sentence.ToLower();
