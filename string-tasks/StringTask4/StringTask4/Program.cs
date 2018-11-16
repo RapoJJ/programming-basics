5ad6191143438bab16b4a09c481a9b0c39887946
udwee <jonttu.92@hotmail.com>
2018-11-16 09:29:11 +0200

    Added summaries to functions.

--- string-tasks/StringTask4/StringTask4/Program.cs
+++ string-tasks/StringTask4/StringTask4/Program.cs
@@ -10,21 +10,28 @@ namespace StringTask4
             string userInput = UserInput();
 
             if (IsPalindrome(userInput))
-            {
                 Console.WriteLine($"Sana {userInput} on palindromi.");
-            }
             else
-            {
                 Console.WriteLine($"Sana {userInput} ei ole palindromi.");
-            }
-
         }
+        /// <summary>
+        /// Asks user for input and saves it as string.
+        /// </summary>
+        /// <returns></returns>
         static string UserInput()
         {
             Console.Write("Syötä lause: ");
             string userInput = Console.ReadLine();
             return userInput;
         }
+        /// <summary>
+        /// Checks if string is a palindrome.
+        /// Deletes empty spaces inbetween words.
+        /// Returns true of false value.
+        /// str = any string
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
         static bool IsPalindrome(string str)
         {
             str = str.ToUpper();
