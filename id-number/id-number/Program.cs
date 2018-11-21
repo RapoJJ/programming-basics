b0c3e194b138b5b34c36c70cbc67cc5a4143605d
udwee <jonttu.92@hotmail.com>
2018-11-21 10:42:41 +0200

    id-number initialized and UserInterface version 1 done

---
+++ id-number/id-number/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace id_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInterface();
+        }
+        static string UserInterface ()
+        {
+            Console.WriteLine("Henkilötunnuksen käsittely");
+            Console.WriteLine("[U] Luo uuden henkilötunnuksen");
+            Console.WriteLine("[T] Tarkistaa henkikötunnuksen oikeellisuuden");
+            Console.WriteLine("[X] Sulkee ohjelman");
+            Console.Write("Valitse mitä tehdään: ");
+
+            return Console.ReadLine();
+        }
+    }
+}
