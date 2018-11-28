59030509cf9dd1c89fff5e1a336dab65b533c35d
udwee <jonttu.92@hotmail.com>
2018-11-28 08:29:52 +0200

    FileHandlingExamples done

---
+++ file-handling/FileHandlingExample/FileClassExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace FileClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines(@"C:\temp\testi.dat");
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
