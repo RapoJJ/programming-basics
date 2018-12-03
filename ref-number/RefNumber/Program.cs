8dc2c9d5f7bdb054d59161896133f146a5fab6ad
udwee <jonttu.92@hotmail.com>
2018-12-03 10:49:29 +0200

    RefNumberChecker function done

--- ref-number/RefNumber/Program.cs
+++ ref-number/RefNumber/Program.cs
@@ -6,19 +6,46 @@ namespace ref_number
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(RefNumberChecker(UserInput()));
+            
 
         }
         static char UserInterface()
         {
             Console.WriteLine("[T] Tarkasta viitenumero.");
             Console.WriteLine("[L] Luo viitenumeroita haluttu määrä.");
-            Console.WriteLine("[x] Sulje ohjelma.");
+            Console.WriteLine("[X] Sulje ohjelma.");
             Console.Write("Syötä valinta:");
 
             return char.ToUpper(Console.ReadKey().KeyChar);
         }
+        static string UserInput()
+        {
+            Console.Write("Syötä viitenumero: ");
+            return Console.ReadLine();
+        }
+        static bool RefNumberChecker(string number)
+        {
+            int[] factor = new int[] { 7, 3, 1 };
+            int sum = 0;
+            int j = number.Length - 2;
 
+            for (int i = 0; i < number.Length - 1; i++)
+            {
+                int nr = Convert.ToInt32(number[j].ToString());
+                sum += nr * factor[i % 3];
+                j--;
+            }
+            int checkNumber = 10 - (sum % 10);
 
-
+            if (checkNumber == Convert.ToInt32(number[number.Length - 1].ToString()))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
