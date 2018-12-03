d97ed3ba94a3168931c4862b00fd1bb4d216397d
udwee <jonttu.92@hotmail.com>
2018-12-03 09:05:33 +0200

    RefNumber initialized and UserInterface done

---
+++ ref-number/RefNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ref_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static char UserInterface()
+        {
+            Console.WriteLine("[T] Tarkasta viitenumero.");
+            Console.WriteLine("[L] Luo viitenumeroita haluttu määrä.");
+            Console.WriteLine("[x] Sulje ohjelma.");
+            Console.Write("Syötä valinta:");
+
+            return char.ToUpper(Console.ReadKey().KeyChar);
+        }
+
+
+
+    }
+}
