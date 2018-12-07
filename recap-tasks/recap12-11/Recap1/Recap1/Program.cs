b82fbfb5776705515ab361d395ea2e325fee13b2
Udwee <jonttu.92@hotmail.com>
2018-12-07 14:40:35 +0200

    Recap1 initialized

---
+++ recap-tasks/recap12-11/Recap1/Recap1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Recap1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tee ohjelma, joka kysyy käyttäjältä lukuja, kunnes hän syöttää kaksi samaa lukua peräkkäin. Ohjelma tulostaa näytölle syötettyjen lukujen summan.");
+        }
+    }
+}
