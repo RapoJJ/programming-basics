c9fd6fa2a5ac8823db6f7ae0b9258982f2180bf5
Udwee <jonttu.92@hotmail.com>
2018-12-07 14:37:31 +0200

    CapitalizeWords initialized

---
+++ recap-tasks/recap3-12/CapitalizeWords/CapitalizeWords/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CapitalizeWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tee ohjelma, joka muuttaa lauseen sanat alkamaan isoilla kirjaimilla.");
+        }
+    }
+}
