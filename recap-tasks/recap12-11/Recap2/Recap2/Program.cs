033697579efd4ec6be584ae3d1acabed32c076d9
Udwee <jonttu.92@hotmail.com>
2018-12-07 14:40:19 +0200

    Recap2 initialized

---
+++ recap-tasks/recap12-11/Recap2/Recap2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Recap2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tee ohjelma, joka arpoo satunnaisluvun väliltä 1-100. " +
+                "Ohjelman käyttäjän pitää arvata luku. " +
+                "Ohjelma antaa vihjeitä, onko arvattu luku liian suuri vai pieni. " +
+                "Jos luku ei ole lukualueella siitä tulee virheilmoitus.");
+        }
+    }
+}
