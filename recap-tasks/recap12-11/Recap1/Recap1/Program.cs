9ea055fc5d1d87d94737a82b5d48fb9f73da6653
Udwee <jonttu.92@hotmail.com>
2018-12-10 12:47:34 +0200

    Recap1 finished

--- recap-tasks/recap12-11/Recap1/Recap1/Program.cs
+++ recap-tasks/recap12-11/Recap1/Recap1/Program.cs
@@ -7,6 +7,38 @@ namespace Recap1
         static void Main(string[] args)
         {
             Console.WriteLine("Tee ohjelma, joka kysyy käyttäjältä lukuja, kunnes hän syöttää kaksi samaa lukua peräkkäin. Ohjelma tulostaa näytölle syötettyjen lukujen summan.");
+
+            int number1 = 0;
+            int number2;
+            int sum = 0;
+
+            while (true)
+            {
+                number2 = UserInput();
+                if (number1 == number2)
+                {
+                    sum += number2;
+                    break;
+                }
+                else
+                {
+                    number1 = number2;
+                    sum += number2;                
+                }
+            }
+            Console.WriteLine($"Syötettyjen lukujen summa: {sum}");
+            
+        }
+        static int UserInput()
+        {
+            while (true)
+            { 
+            Console.Write("Syötä luku: ");
+            bool isNumber = int.TryParse(Console.ReadLine(), out int number);
+
+            if (isNumber)
+                return number;
+            }
         }
     }
 }
