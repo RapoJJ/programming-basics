5c8adaf58871f97173f698d5f3e9595189253d69
Udwee <jonttu.92@hotmail.com>
2018-12-10 12:59:13 +0200

    Recap2 finished

--- recap-tasks/recap12-11/Recap2/Recap2/Program.cs
+++ recap-tasks/recap12-11/Recap2/Recap2/Program.cs
@@ -10,6 +10,43 @@ namespace Recap2
                 "Ohjelman käyttäjän pitää arvata luku. " +
                 "Ohjelma antaa vihjeitä, onko arvattu luku liian suuri vai pieni. " +
                 "Jos luku ei ole lukualueella siitä tulee virheilmoitus.");
+
+            Random rnd = new Random();
+            int guessNumber = rnd.Next(100);
+            while (true)
+            {
+                int userNumber = UserInput();
+
+                if (userNumber < 1 || userNumber > 100)
+                {
+                    Console.WriteLine("Syöttämäsi luku ei ole alueelta 1-100.");
+                }
+                else if (userNumber == guessNumber)
+                {
+                    Console.WriteLine("Arvasit oikein!");
+                    break;
+                }
+                else if (userNumber < guessNumber)
+                {
+                    Console.WriteLine("Arvattu luku on liian pieni.");
+                }
+                else if (userNumber > guessNumber)
+                {
+                    Console.WriteLine("Arvattu luku on liian suuri.");
+                }
+            }
+            Console.WriteLine($"Oikea luku oli {guessNumber}.");
+        }
+        static int UserInput()
+        {
+            while (true)
+            {
+                Console.Write("Arvaa luku: ");
+                bool isNumber = int.TryParse(Console.ReadLine(), out int number);
+
+                if (isNumber)
+                    return number;
+            }
         }
     }
 }
