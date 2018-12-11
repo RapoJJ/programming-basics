5065931a843da789102696e846f655042f98d5bd
Udwee <jonttu.92@hotmail.com>
2018-12-11 22:24:46 +0200

    ArrayRecap1 done

--- recap-tasks/recap-arrays/ArrayRecap1/ArrayRecap1/Program.cs
+++ recap-tasks/recap-arrays/ArrayRecap1/ArrayRecap1/Program.cs
@@ -8,7 +8,26 @@ namespace ArrayRecap1
         {
             Console.WriteLine("Tee funktio, joka palauttaa parametrina annetun määrän tähtiä.");
 
-            //Lisää pääohjelmaan taulukko int[] numbers = new int[] { 2, 5, 10, 7, 3 };
+            int[] numbers = new int[] { 2, 5, 10, 7, 3 };
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(StarPrinter(numbers[i]));
+            }
+        }
+        /// <summary>
+        /// Returns a string with stars, int amount defines how many stars the string holds.
+        /// </summary>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        static string StarPrinter(int amount)
+        {
+            string stars = "";
+            for (int i = 0; i < amount; i++)
+            {
+                stars += "*";
+            }
+            return stars;
         }
     }
 }
