8e145aad56d552c150e698769b93ebe754cc51d2
Udwee <jonttu.92@hotmail.com>
2018-12-11 22:47:07 +0200

    ArrayRecap2 done

--- recap-tasks/recap-arrays/ArrayRecap2/ArrayRecap2/Program.cs
+++ recap-tasks/recap-arrays/ArrayRecap2/ArrayRecap2/Program.cs
@@ -14,6 +14,22 @@ namespace ArrayRecap2
             //MacBook Air   928,90€ 
             //Jolla         349,00€ 
             //Nokia         99,80€
+            string[] items = new string[3] { "Macbook Air", "Jolla", "Nokia" };
+            string[] prices = new string[3] { "928,90€", "349,00€", "99,80€" };
+
+            ArrayPrinter(items, prices);
+        }
+        /// <summary>
+        /// Prints info from two arrays.
+        /// </summary>
+        /// <param name="names"></param>
+        /// <param name="euros"></param>
+        static void ArrayPrinter(string[] names, string[] euros)
+        {
+            for (int i = 0; i < names.Length; i++)
+            {
+                Console.WriteLine($"{names[i]}\t{euros[i]}");
+            }
         }
     }
 }
